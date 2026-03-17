694b7663015bdaae861fe53b54de63d689a942e7
Spoorthi S Navali <bhoominavali12@gmail.com>
2026-03-17 16:43:38 +0530

    Create Day-1.py
    Created a program to take student details and calculated total and average marks using variables ,inputs and print statements.

---
+++ Day-1.py
@@ -0,0 +1,10 @@
+#Student  marks Analysis.
+name=input("Enter the name of student\n")
+Roll_number=int(input("Enter the rollnumber of student\n"))
+marks1=int(input("Enter the marks scored in 1st subject")) 
+marks2=int(input("Enter the marks scored in 2nd subject"))
+marks3=int(input("Enter the marks scored in 3rd subject"))
+total=marks1+marks2+marks3
+avg=total/3
+print(total)
+print(avg)
