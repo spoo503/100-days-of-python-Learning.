6423904d929c81b242d26e9cf9382ca3ca479914
Spoorthi S Navali <bhoominavali12@gmail.com>
2026-04-14 14:34:39 +0530

    Update Medium_question.py

--- Medium_question.py
+++ Medium_question.py
@@ -126,3 +126,91 @@ Reverse a list.
 
 Find second largest number in list.
 Find common elements between two lists.
+
+
+SIMPLE CALCULATOR:
+class simple_calculator:
+    def __init__(self,Add,sub,multi,Div,Exit):
+        self.Add=Add
+        self.sub=sub
+        self.multi=multi
+        self.Div=Div
+        self.Exit=Exit
+    def Addition(self,a,b):
+        print(a+b)
+    def Subtraction(self,a,b):
+        print(a-b)
+    def Multiplication(self,a,b):
+        print(a*b)
+    def Division(self,a,b):
+        if(b==0):
+            print("A canno be divided by b")
+        else:
+            print(a/b)
+    def Exit_for(self):
+        print("YOu have exited the from the calculator")
+calc=simple_calculator(0,0,0,0,0)
+while True:
+    print("###Simple calculator###")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Exit")
+    option=int(input("Enter the operation"))
+    if(option==5):
+        calc.Exit_for()
+        break
+    a=int(input("Enter the first number"))
+    b=int(input("Enter the second number"))
+    if(option==1):
+        calc.Addition(a,b)
+    elif(option==2):
+        calc.Subtraction(a,b)
+    elif(option==3):
+        calc.Multiplication(a,b)
+    elif(option==4):
+        calc.Division(a,b)
+    else:
+        print("Invalid supporting")
+
+
+BANK ACCOUNT:
+class Bank:
+    def __init__(self,balance,amount):
+        self.balance=balance
+        self.amount=amount
+    def check_balance(self):
+        print(self.balance)
+    def Deposit(self):
+        self.balance+=self.amount
+        print(self.balance)
+    def Withdraw(self):
+        if(self.amount>self.balance):
+            print("Insufficient balance")
+        else:
+            self.balance-=self.amount
+            print(self.balance)
+bank=Bank(0,0)
+while True:
+    print("1.Check_BALAANCE")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Quit")
+    option=int(input("Enter the option"))
+    amount=int(input("ENter the amount"))
+    balance=int(input("ENter the BALANCE"))
+    bank.amount=amount
+    bank.balance=balance
+    if(option==4):
+        print("Exiting")
+        break
+    elif(option==1):
+         bank.check_balance()
+    elif(option==2):
+        bank.Deposit()
+    elif(option==3):
+        bank.Withdraw()
+    else:
+        print("INvalid")
+    
